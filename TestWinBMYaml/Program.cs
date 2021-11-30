573dbeba987371b4f45975d58017d966dde53478
tgiqfe <tqji31001@gmail.com>
2021-11-30 09:25:37 +0900

    Add Recipe classes.

--- TestWinBMYaml/Program.cs
+++ TestWinBMYaml/Program.cs
@@ -1,5 +1,14 @@
-﻿
+﻿using WinBM.Recipe;
+
 string targetPath = "Test.yml";
+
+List<Page> list = Page.Deserialize(targetPath);
+
+
+foreach (var page in list)
+{
+    Console.WriteLine(page.Serial);
+}
 
 
 Console.ReadLine();
